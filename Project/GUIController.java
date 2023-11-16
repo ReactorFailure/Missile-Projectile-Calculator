277d938d5edf565bffcf0a59147bd51d41ccf5e5
Zeyu Huang <zeze33629@gmail.com>
2023-11-16 17:04:44 -0500

    Added the things to the controller... Missing the Animation pane stuff

--- Project/GUIController.java
+++ Project/GUIController.java
@@ -2,7 +2,12 @@ package Project;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
 public class GUIController {
 
@@ -11,6 +16,63 @@ public class GUIController {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Button btn_Clear;
+
+    @FXML
+    private Button btn_Clear1;
+
+    @FXML
+    private Button btn_Launch;
+
+    @FXML
+    private Button btn_Launch1;
+
+    @FXML
+    private Button btn_Switch;
+
+    @FXML
+    private Button btn_Switch1;
+
+    @FXML
+    private Slider slider_AngleOfLaunch;
+
+    @FXML
+    private Slider slider_DistanceToTarget;
+
+    @FXML
+    private Slider slider_HeightOfLaunch;
+
+    @FXML
+    private Slider slider_InitialVelocity;
+
+    @FXML
+    private TextField tf_AngleOfLaunch;
+
+    @FXML
+    private TextField tf_DistanceToTarget;
+
+    @FXML
+    private TextField tf_HeightOfLaunch;
+
+    @FXML
+    private TextField tf_InitialVelocity;
+
+    @FXML
+    void ClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LaunchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchOnAction(ActionEvent event) {
+
+    }
 
     @FXML
     void initialize() {
