30531e413e27418c9169aa9f0cda6170a2128141
ReactorFailure <69980969+ReactorFailure@users.noreply.github.com>
2023-11-22 20:43:47 -0500

    Made Physics constructor and setters and getters
    MR BEAST

--- Project/Physics.java
+++ Project/Physics.java
@@ -4,7 +4,62 @@ import java.util.ArrayList;
 
 public class Physics {
 
+    private double angleOfLaunch; //angle of launch
+    private double gravAcceleration; //gravitational acceleration
+    private double heightOfLaunch; //height of launch
+    private double initialVelocity; //initial velocity
+
+
     public Physics(ArrayList<Double> launchValues) {
+        angleOfLaunch = launchValues.indexOf(0);
+        gravAcceleration = launchValues.indexOf(1);
+        heightOfLaunch = launchValues.indexOf(2);
+        initialVelocity = launchValues.indexOf(3);
     }
-    
+
+    //Getter and Setter for angleOfLaunch
+    public double getAngleOfLaunch() {
+        return angleOfLaunch;
+    }
+    public void setAngleOfLaunch(double angleOfLaunch) {
+        this.angleOfLaunch = angleOfLaunch;
+    }
+
+    //Getter and Setter for gravAcceleration
+    public double getGravAcceleration() {
+        return gravAcceleration;
+    }
+    public void setGravAcceleration(double gravAcceleration) {
+        this.gravAcceleration = gravAcceleration;
+    }
+
+
+
+    public double getHeightOfLaunch() {
+        return heightOfLaunch;
+    }
+
+    public void setHeightOfLaunch(double heightOfLaunch) {
+        this.heightOfLaunch = heightOfLaunch;
+    }
+
+
+    public double getInitialVelocity() {
+        return initialVelocity;
+    }
+
+    public void setInitialVelocity(double initialVelocity) {
+        this.initialVelocity = initialVelocity;
+    }
+
+    public double calcMissileTime() {
+        //Turning getters to physics variables
+        double angle = getAngleOfLaunch();
+        double a = getGravAcceleration();
+        double vi = getInitialVelocity();
+
+        return (-(vi*Math.sin(angle))-(vi*Math.sin(angle)))/a;
+        //V_fy = V_iy + a*t (simplified to isolate t)
+    }
+
 }
