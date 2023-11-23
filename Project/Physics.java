ee49cebd721ff852a81bd31f863973ad353edd79
ReactorFailure <69980969+ReactorFailure@users.noreply.github.com>
2023-11-22 21:32:42 -0500

    Added more stuff for Physics file
    I sure hope i can fix javafx so i can help with ui

--- Project/Physics.java
+++ Project/Physics.java
@@ -10,6 +10,7 @@ public class Physics {
     private double initialVelocity; //initial velocity
 
 
+    //Can't believe I had to review my projectile motion for this lol
     public Physics(ArrayList<Double> launchValues) {
         angleOfLaunch = launchValues.indexOf(0);
         gravAcceleration = launchValues.indexOf(1);
@@ -52,14 +53,34 @@ public class Physics {
         this.initialVelocity = initialVelocity;
     }
 
-    public double calcMissileTime() {
+    //Calculate time until the missile hits target
+    public double calcTime() {
         //Turning getters to physics variables
         double angle = getAngleOfLaunch();
         double a = getGravAcceleration();
         double vi = getInitialVelocity();
 
-        return (-(vi*Math.sin(angle))-(vi*Math.sin(angle)))/a;
-        //V_fy = V_iy + a*t (simplified to isolate t)
+        return ((-(vi*Math.sin(angle))-(vi*Math.sin(angle)))/a);
+        // v_fy = v_iy + a*t    Simplified to isolate t
     }
 
+    //Calculate max height missile will reach
+    public double calcMaxHeight() {
+        double angle = getAngleOfLaunch();
+        double a = getGravAcceleration();
+        double vi = getInitialVelocity();
+        double yi = getHeightOfLaunch();
+
+        return ((((vi*Math.sin(angle))*(vi*Math.sin(angle)))/2*a)+yi);
+        // (v_fy)^2 = (v_iy)^2 - 2*a*(y_f - y_i)   Simplified to isolate y_f. v_fy is zero since we are calculating max height
+    }
+
+    //Calculate distance of missile at y=0
+    public double calcDistance() {
+        double angle = getAngleOfLaunch();
+        double vi = getInitialVelocity();
+
+        return (vi*Math.cos(angle))*calcTime();
+        // x_f = x_i + v_x*t    x_i is zero because that's the starting point and because of reference frames and what not
+    }
 }
