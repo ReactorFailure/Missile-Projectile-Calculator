694b8f74dec708665d650c438b495d8693e1872a
Zeyu Huang <zeze33629@gmail.com>
2023-11-26 16:39:43 -0500

    Animation working

--- Project/AnimationSceneController.java
+++ Project/AnimationSceneController.java
@@ -1,9 +1,11 @@
 package Project;
 
+import javafx.animation.PathTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.QuadCurve;
+import javafx.util.Duration;
 
 public class AnimationSceneController {
     Physics physics;
@@ -22,6 +24,17 @@ public class AnimationSceneController {
 
     public void setPhysics(Physics physics) {
         this.physics = physics;
-        System.out.println(physics.getAngleOfLaunch());
+    }
+
+    public void animation() {
+        // Path path = new Path();
+        // path.getElements().add(new MoveTo(50, 500));
+        // path.getElements().add(new QuadCurveTo(300, 150, 550, 500));
+
+        PathTransition transition = new PathTransition(Duration.seconds(10), path);
+        transition.setNode(rocket_Iv);
+        transition.setCycleCount(1);
+        transition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        transition.play();
     }
 }
--- Project/GUIController.java
+++ Project/GUIController.java
@@ -3,7 +3,6 @@ package Project;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,9 +17,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.QuadCurve;
-import javafx.scene.shape.QuadCurveTo;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class GUIController {
     // An array list of textfield and slider
@@ -105,9 +102,10 @@ public class GUIController {
     @FXML
     // Starts the animation
     void launchOnAction(ActionEvent event) throws IOException {
-        // updateOnAction(event);
         startAnimation(event);
-        // animation();
+        
+        // Disable launch btn
+        btn_Launch.setDisable(true);
     }
 
     @FXML
@@ -147,7 +145,6 @@ public class GUIController {
 
     @FXML
     void initialize() {
-        // System.out.println(ta_Messages.getX());
         // Arraylist of slider
         array_Sliders.add(slider_AngleOfLaunch);
         array_Sliders.add(slider_GravitationalAcceleration);
@@ -220,39 +217,21 @@ public class GUIController {
         btn_Launch.setDisable(false);
     }
 
-    public void animation() {
-        PathTransition transition = new PathTransition(Duration.seconds(10), path);
-        // transition.setPath(path);
-        // System.out.println(y_Axis.getStartY());
-        // System.out.println(y_Axis.getEndY());
-        // System.out.println(x_Axis.getStartY());
-        transition.setNode(rocket_Iv);
-        transition.setCycleCount(1);
-        transition.play();
-    }
-
-    // // Updates the pane and the physics object
-    // public void update() {
-    // // Check if its in textfield mode
-    // if (pane_TfMode.isVisible()) {
-    // tfMode();
-    // }
-    // }
-
     // Creates another scene and makes it appear on stage
     private void startAnimation(ActionEvent e) throws IOException {
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("AnimationScene.fxml"));
-
         Parent root = loader.load();
 
         // Retrieve the controller after loading
         AnimationSceneController controller = loader.getController();
         controller.setPhysics(physics);
+        controller.animation();
 
         stage.setScene(new Scene(root));
         stage.setTitle("Animation running");
+        stage.setResizable(false);
         stage.show();
     }
 }
